81b52240ecdd588b7e468227300e2f790860085d
KulokovaSasha <kulikova.sasha@gmail.com>
2026-03-04 15:23:05 +0300

    Day 3 - Telegram bot connected to FastAPI server

--- week1/telegram_bot.py
+++ week1/telegram_bot.py
@@ -7,16 +7,16 @@ import os
 load_dotenv()  # загружает переменные из .env
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-from telegram import Bot
-bot = Bot(token=TOKEN)
 
 async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
     async with httpx.AsyncClient(verify=False) as client:
         response = await client.get("http://127.0.0.1:8000/quote")
         data = response.json()
+
     await update.message.reply_text(f'"{data["quote"]}" — {data["author"]}')
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("quote", quote))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
